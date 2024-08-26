eedd4fa7297a298cc987003c37ee6f05ff6b3421
shaikhjavedofficial <skjaved633@gmail.com>
2024-08-26 16:50:48 +0530

    skills

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+{
+  /* <Typography
+            color="#60ACE6"
+            variant="body1"
+            bottom="1rem"
+            position="relative"
+            // left="3rem"
+          >
+            &#169; Copyright Shaikh Javed 2024.
+          </Typography> */
+}
