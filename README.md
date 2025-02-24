79ba448f56532dceef5abb0919d84f2e8f919288
Tastefultrendsltd <Tastefultrendsltd@gmail.com>
2025-02-24 14:07:42 +0530

    init

--- README.md
+++
@@ -1,1 +0,0 @@
-portfolio webapp using crisp template
--- src/Utils/ASSETS.ts
+++ src/Utils/ASSETS.ts
@@ -1,30 +1,29 @@
-import ach1 from "../../public/assets/ach1.jpg";
-import ach2 from "../../public/assets/ach2.jpg";
-import ach3 from "../../public/assets/ach3.jpg";
-import ach4 from "../../public/assets/ach4.jpg";
-// export const MYIMG = [
-//     {
-//     src:new URL("~/public/assets/my_img.jpg", import.meta.url).href,
-//     alt: 'my image',
-//     as: 'image/jpg'
-//         // as: 'image/svg+xml'
-//         // as: 'image/png'
-//     }
-// ]
+// import ach1 from "../../public/assets/ach1.jpg";
+// import ach2 from "../../public/assets/ach2.jpg";
+// import ach3 from "../../public/assets/ach3.jpg";
+// import ach4 from "../../public/assets/ach4.jpg";
 
-export const ACH1 = {
-  src: ach1,
-  alt: "ach1",
-};
-export const ACH2 = {
-  src: ach2,
-  alt: "ach2",
-};
-export const ACH3 = {
-  src: ach3,
-  alt: "ach3",
-};
-export const ACH4 = {
-  src: ach4,
-  alt: "ach4",
-};
+// export const ACH1 = {
+//   src: ach1,
+//   alt: "ach1",
+// };
+// export const ACH2 = {
+//   src: ach2,
+//   alt: "ach2",
+// };
+// export const ACH3 = {
+//   src: ach3,
+//   alt: "ach3",
+// };
+// export const ACH4 = {
+//   src: ach4,
+//   alt: "ach4",
+// };
+
+//add these into manifest.json
+// "resources": [
+//   "assets/ach1.jpg",
+//   "assets/ach2.jpg",
+//   "assets/ach3.jpg",
+//   "assets/ach4.jpg"
+// ],
